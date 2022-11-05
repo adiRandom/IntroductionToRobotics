3d807aed2751079f64f496cf704dd029e5f3223f
adi_random <me.adi.hf@gmail.com>
2022-11-05 18:05:31 +0200

    Reset on long press

--- tema_3/tema_3.ino
+++ tema_3/tema_3.ino
@@ -18,19 +18,29 @@ uint8_t const DISPLAY_PINS[DISPLAY_PINS_SIZE] = {
   DISPLAY_PIN_DP
 };
 
+uint8_t const DISPLAY_PIN_A_INDEX = 0;
+uint8_t const DISPLAY_PIN_B_INDEX = 1;
+uint8_t const DISPLAY_PIN_C_INDEX = 2;
+uint8_t const DISPLAY_PIN_D_INDEX = 3;
+uint8_t const DISPLAY_PIN_E_INDEX = 4;
+uint8_t const DISPLAY_PIN_F_INDEX = 5;
+uint8_t const DISPLAY_PIN_G_INDEX = 6;
+uint8_t const DISPLAY_PIN_DP_INDEX = 7;
+
+
 // The first dimension is the display led pin index
 // The second dimension is the led pin index that shold be selected if we move into the direction indicated by the second index from the pin indicated by the first index
 // The indexing of the second dimension coresponds to the Direction enum
 // -1 indicates we can't select any display led in that direction
 int8_t const LED_MOVE_GRAPH[DISPLAY_PINS_SIZE][4] = {
-  { -1, 6, 5, 1 },
-  { -1, 2, 0, -1 },
-  { 1, -1, 3, 7 },
-  { 6, -1, 4, 2 },
-  { 5, -1, -1, 3 },
-  { -1, 4, -1, 0 },
-  { 0, 3, -1, -1 },
-  { -1, -1, 2, -1 },
+  { -1, DISPLAY_PIN_G_INDEX, DISPLAY_PIN_F_INDEX, DISPLAY_PIN_B_INDEX },
+  { -1, DISPLAY_PIN_C_INDEX, DISPLAY_PIN_A_INDEX, -1 },
+  { DISPLAY_PIN_B_INDEX, -1, DISPLAY_PIN_D_INDEX, DISPLAY_PIN_DP_INDEX },
+  { DISPLAY_PIN_G_INDEX, -1, DISPLAY_PIN_E_INDEX, DISPLAY_PIN_C_INDEX },
+  { DISPLAY_PIN_F_INDEX, -1, -1, DISPLAY_PIN_D_INDEX },
+  { -1, DISPLAY_PIN_E_INDEX, -1, DISPLAY_PIN_A_INDEX },
+  { DISPLAY_PIN_A_INDEX, DISPLAY_PIN_D_INDEX, -1, -1 },
+  { -1, -1, DISPLAY_PIN_C_INDEX, -1 },
 };
 
 uint8_t const JOY_X_AXIS_PIN = A0;
@@ -41,12 +51,15 @@ uint8_t const JOY_BTN_PIN = 12;
 uint16_t const POSITIVE_THRESHOLD = 700;
 uint16_t const NEGATIVE_THRESHOLD = 300;
 uint8_t const INPUT_DEBOUNCE_TIME = 50;
-
+uint32_t const LONG_PRESS_TIME = 1500;
 uint16_t const BLINK_INTERVAL = 500;
 
 // True if the X axis should be interpreted as the Y axis and vice versa
 // False otherwise
-bool invertedAxes = true;
+bool INVERTED_AXES = true;
+// The value emitted by the button when pressed
+byte BUTTON_PRESSED = LOW;
+
 bool shouldReadJoyDir = true;
 // When we disable joy reading, we keep track of the axis we are currently reading.
 // When that axis gets into the neutral position, we re-enable reading
@@ -62,6 +75,7 @@ bool canCurrentLedMove = true;
 byte lastButtonReadingState = HIGH;
 byte joyBtnState = HIGH;
 uint32_t lastButtonReadingDebounce = 0;
+uint32_t joyBtnPressTime = 0;
 
 enum Direction {
   UP,
@@ -81,10 +95,14 @@ void pinSetup() {
   pinMode(JOY_BTN_PIN, INPUT_PULLUP);
 }
 
-void setup() {
-  pinSetup();
+void resetDisplay() {
+  for (uint8_t i = 0; i < DISPLAY_PINS_SIZE; i++) {
+    ledState[i] = LOW;
+  }
 
-  Serial.begin(9600);
+  currentLedPinIndex = DISPLAY_PIN_DP_INDEX;
+  shouldCurrentLedBlink = true;
+  canCurrentLedMove = true;
 }
 
 // @Returns 1 for the positive semi-axis,
@@ -123,25 +141,24 @@ int8_t getJoyValueOnAxis(uint8_t pin) {
 }
 
 Direction getJoyDirection() {
-
   // We first read the X axis then the Y axis
   // If we register input on the X axis first, we return a direction on that axis
   // When we get a reading on an axis, we block further reading until the joystick gets back to the neutral position first
   int8_t xAxisVal = getJoyValueOnAxis(JOY_X_AXIS_PIN);
 
   if (xAxisVal == 1) {
-    return invertedAxes ? Direction::UP : Direction::RIGHT;
+    return INVERTED_AXES ? Direction::UP : Direction::RIGHT;
   } else if (xAxisVal == -1) {
-    return invertedAxes ? Direction::DOWN : Direction::LEFT;
+    return INVERTED_AXES ? Direction::DOWN : Direction::LEFT;
   }
 
   // We haven't returned yet so we can process the Y axis
   int8_t yAxisVal = getJoyValueOnAxis(JOY_Y_AXIS_PIN);
 
   if (yAxisVal == 1) {
-    return invertedAxes ? Direction::RIGHT : Direction::UP;
+    return INVERTED_AXES ? Direction::RIGHT : Direction::UP;
   } else if (yAxisVal == -1) {
-    return invertedAxes ? Direction::LEFT : Direction::DOWN;
+    return INVERTED_AXES ? Direction::LEFT : Direction::DOWN;
   }
 
   return Direction::NONE;
@@ -167,23 +184,37 @@ void showLeds() {
   }
 }
 
-void readButtonState(void (*onStateChange)(byte), uint8_t notifyOn = CHANGE) {
+void readButtonState(void (*onStateChange)(byte, bool), uint8_t notifyOn = CHANGE) {
   byte buttonReading = digitalRead(JOY_BTN_PIN);
+  uint32_t now = millis();
 
   if (buttonReading != lastButtonReadingState) {
-    lastButtonReadingDebounce = millis();
+    lastButtonReadingDebounce = now;
   }
 
-  if (millis() - lastButtonReadingDebounce > INPUT_DEBOUNCE_TIME) {
+  if (now - lastButtonReadingDebounce > INPUT_DEBOUNCE_TIME) {
     if (buttonReading != joyBtnState) {
       joyBtnState = buttonReading;
+      bool isLongPress = false;
+
+      if (joyBtnState == BUTTON_PRESSED) {
+        // Start timer for long press
+        joyBtnPressTime = now;
+      } else {
+        Serial.println(now - joyBtnPressTime);
+        if (now - joyBtnPressTime >= LONG_PRESS_TIME) {
+          isLongPress = true;
+        }
+
+        joyBtnPressTime = 0;
+      }
 
       if (
         (notifyOn == CHANGE)
         || (notifyOn == RISING && joyBtnState == HIGH)
         || (notifyOn == FALLING && joyBtnState == LOW)
       ) {
-        onStateChange(joyBtnState);
+        onStateChange(joyBtnState, isLongPress);
       }
     }
   }
@@ -191,7 +222,13 @@ void readButtonState(void (*onStateChange)(byte), uint8_t notifyOn = CHANGE) {
   lastButtonReadingState = buttonReading;
 }
 
-void onButtobStateChange(byte state) {
+void onButtobStateChange(byte state, bool isLongPress) {
+
+  if (isLongPress) {
+    resetDisplay();
+    return;
+  }
+
   if (canCurrentLedMove) {
     // We are transitioning in selection state
     // Set the current led to the last blinking state
@@ -218,7 +255,7 @@ void chnageCurrentLedStateOnJoyMove(Direction dir) {
 
 void handleInput() {
   Direction dir = getJoyDirection();
-  readButtonState(onButtobStateChange, FALLING);
+  readButtonState(onButtobStateChange, RISING);
 
   if (dir == Direction::NONE) {
     return;
@@ -228,6 +265,12 @@ void handleInput() {
   } else {
     chnageCurrentLedStateOnJoyMove(dir);
   }
+}
+
+void setup() {
+  pinSetup();
+
+  Serial.begin(9600);
 }
 
 void loop() {
